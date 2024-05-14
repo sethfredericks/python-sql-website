e5cb9dfd2d9a94280f7d7aeb4648b790297185ae
Seth Fredericks <100998449+sethfredericks@users.noreply.github.com>
2024-05-14 18:06:10 +0000

    add requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'id' : 1,
+  'title': 'Data Analyst',
+  'location': 'Johannesburg, South Africa',
+  'salary': 'R650 000'
+  },
+  {
+    'id': 2,
+    'title': 'Frontend Engineer',
+    'location': 'Cape Town, South Africa',
+    'salary': 'R800 000'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Cape Town, South Africa',
+    'salary': 'R850 000'
+  },
+  {
+    'id': 2,
+    'title': 'ML/AI Engineer',
+    'location': 'Cape Town, South Africa',
+    'salary': 'R1 200 000'
+  }
+]
+
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('index.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
